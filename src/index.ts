0eb38a50cc9387ed4189caf1cb09a0a995053771
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-05 14:34:05 -0300

    :sparkles: feat: Create server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: "bar" });
+});
+
+const port = 8089;
+app.listen(port, () => {
+  console.log(`==> Running on port:.. ${port}`);
+});
